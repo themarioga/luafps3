7bc16cc71d2e794cbcf6e9c72741881ba4552f40
marioga2512@gmail.com <marioga2512@gmail.com@eef860d8-15e4-3f82-2c55-e5074b3e00fb>
2011-09-22 21:23:18 +0000

    Repaired Script.lua

--- trunk/docs/script.lua
+++ trunk/docs/script.lua
@@ -1,115 +1,85 @@
-negro = Screen.color(0,0,0,0)
-fondo = Screen.imageLoad("fondo.png")
-cd = Screen.imageLoad("cd.png")
-musictype = 0
+negro = Screen.color(0,0,0)
 i=0
-y=35
+y=30
 sel=0
-reprod=""
 while true do
-	wpad1 = Controls.read(0)
+	pad = Controls.read(0)
 	System.getDir()
-	Screen.imageBlit(1, 1, fondo)
 	maxd = System.getMaxDir() - 1
+	Screen.print(30, 300, "El directorio actual es: "..System.getPathDir()..". En el hay "..maxd.." archivos y carpetas")
 	while i <= maxd do
-		if System.getDType(i) == 0 then
+		if System.getType(i) == 0 then
 			if i == sel then
-				Screen.print(5, y, "->"..System.getDName(i))
+				Screen.print(30, y, "->"..System.getName(i))
 			else
-				Screen.print(5, y, System.getDName(i))
+				Screen.print(30, y, System.getName(i))
 			end
 		else
 			if i == sel then
-				Screen.print(5, y, "-><D>"..System.getDName(i))
+				Screen.print(30, y, "-><D>"..System.getName(i))
 			else
-				Screen.print(5, y, "<D>"..System.getDName(i))
+				Screen.print(30, y, "<D>"..System.getName(i))
 			end
 		end
 		i=i+1
 		y=y+15
-		Screen.flip()
 	end
-	if Controls.pressUp(wpad1) == 1 and sel > 0 then
+	Screen.flip()
+	if Controls.up(pad) == 1 and sel > 0 then
 		sel=sel-1
 		i=0
 		y=35
+		Screen.clear(negro)
 	end
-	if Controls.pressDown(wpad1) == 1 and sel < maxd then
+	if Controls.down(pad) == 1 and sel < maxd then
 		sel=sel+1
 		i=0
 		y=35
+		Screen.clear(negro)
 	end
-	if Controls.pressA(wpad1) == 1 then
-		if System.getDType(sel) == 1 then
+	if Controls.cross(pad) == 1 then
+		Screen.clear(negro)
+		if System.getType(sel) == 1 then
 			System.getDir(sel)
 			sel = 0
 		else
-			curdir = System.getPathDir().."/"..System.getDName(sel)
-			if System.isDExt(".PNG", sel) == 1 or System.isDExt(".png", sel) == 1 then
+			curdir = System.getPathDir().."/"..System.getName(sel)
+			if System.isExt(".PNG", sel) == 1 or System.isExt(".png", sel) == 1 then
 				Screen.imageLoadinOne(1, 1, curdir)
 				Screen.flip()
 				Screen.delay(5)
-			elseif System.isDExt(".JPG", sel) == 1 or System.isDExt(".jpg", sel) == 1 then
+			elseif System.isExt(".JPG", sel) == 1 or System.isExt(".jpg", sel) == 1 then
 				Screen.imageLoadinOne(1, 1, curdir)
 				Screen.flip()
 				Screen.delay(5)
-			elseif System.isDExt(".JPEG", sel) == 1 or System.isDExt(".jpeg", sel) == 1 then
+			elseif System.isExt(".JPEG", sel) == 1 or System.isExt(".jpeg", sel) == 1 then
 				Screen.imageLoadinOne(1, 1, curdir)
 				Screen.flip()
 				Screen.delay(5)
-			elseif System.isDExt(".BMP", sel) == 1 or System.isDExt(".bmp", sel) == 1 then
+			elseif System.isExt(".BMP", sel) == 1 or System.isExt(".bmp", sel) == 1 then
 				Screen.imageLoadinOne(1, 1, curdir)
 				Screen.flip()
 				Screen.delay(5)
-			elseif System.isDExt(".TIF", sel) == 1 or System.isDExt(".tif", sel) == 1 then
+			elseif System.isExt(".TIF", sel) == 1 or System.isExt(".tif", sel) == 1 then
 				Screen.imageLoadinOne(1, 1, curdir)
 				Screen.flip()
 				Screen.delay(5)
-			elseif System.isDExt(".TIFF", sel) == 1 or System.isDExt(".tiff", sel) == 1 then
+			elseif System.isExt(".TIFF", sel) == 1 or System.isExt(".tiff", sel) == 1 then
 				Screen.imageLoadinOne(1, 1, curdir)
 				Screen.flip()
 				Screen.delay(5)
-			elseif System.isDExt(".GIF", sel) == 1 or System.isDExt(".gif", sel) == 1 then
+			elseif System.isExt(".GIF", sel) == 1 or System.isExt(".gif", sel) == 1 then
 				Screen.imageLoadinOne(1, 1, curdir)
 				Screen.flip()
 				Screen.delay(5)
-			elseif System.isDExt(".MP3", sel) == 1 or System.isDExt(".mp3", sel) == 1 then
-				Sound.mp3LoadandPlay(curdir,0)
-				musictype=1
-			elseif System.isDExt(".WAV", sel) == 1 or System.isDExt(".wav", sel) == 1 then
-				Sound.wavLoadandPlay(curdir)
-				musictype=2
-			elseif System.isDExt(".OGG", sel) == 1 or System.isDExt(".ogg", sel) == 1 then
-				Sound.oggLoadandPlay(curdir, 0, 0)
-				musictype=3
-			elseif System.isDExt(".LUA", sel) == 1 or System.isDExt(".lua", sel) == 1 then
+			elseif System.isExt(".LUA", sel) == 1 or System.isExt(".lua", sel) == 1 then
 				dofile(curdir)
 			end
 		end
 		i=0
 		y=35
 	end
-	while musictype ~= 0 do
-		wpad1 = Controls.read(0)
-		Screen.imageBlit(120, 50, cd)
-		reprod = System.getDName(sel)
-		Screen.print(5, 460, "Se esta reproduciendo: "..reprod.." Pulsa B para parar")
-		Screen.flip()
-		if Controls.pressB(wpad1) == 1 then
-			if musictype == 1 then
-				Sound.mp3Stop()
-				musictype = 0
-			elseif musictype == 3 then
-				Sound.oggStop()
-				musictype = 0
-			end
-			musictype = 0
-			i=0
-			y=35
-		end
-	end
-	if Controls.pressHome(wpad1) == 1 then
-		System.exitHBC()
+	if Controls.circle(pad) == 1 then
+		System.exit()
 	end
 end
->>>>>>> .r2
